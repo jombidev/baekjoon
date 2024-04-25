7d4ad0bef171a34d6a027d165a8356177469569f
jombi <jombi@duck.com>
2024-04-25 17:31:58 +0900

    24/04/25 - 2 more silvers

---
+++ src/main/kotlin/solved/bj1347.kt
@@ -0,0 +1,51 @@
+package solved
+
+fun main() {
+    readln()
+    val o = Array(100) { Array(100) { '#' } }
+    var startX = 50
+    var startY = 50 // idk where hong jun is
+    var minX = minOf(101, startX);var minY = minOf(101, startY);var maxX = maxOf(-1, startX);var maxY = maxOf(-1, startY)
+
+    o[startY][startX] = '.'
+    var facing = Facing.DOWN
+    for (c in readln()) {
+        when (c) {
+            'L' -> facing = facing.left()
+            'R' -> facing = facing.right()
+            'F' -> {
+                startX += facing.offsetY
+                startY += facing.offsetX
+                minX = minOf(minX, startX)
+                minY = minOf(minY, startY)
+                maxX = maxOf(maxX, startX)
+                maxY = maxOf(maxY, startY)
+                o[startX][startY] = '.'
+            }
+        }
+    }
+    for (i in minX..maxX) {
+        for (j in minY..maxY) {
+            print(o[i][j])
+        }
+        println()
+    }
+}
+
+enum class Facing(val offsetX: Int, val offsetY: Int) {
+    UP(0, -1), DOWN(0, 1),
+    LEFT(-1, 0), RIGHT(1, 0)
+    ;
+    fun left() = when (this) {
+        UP -> LEFT
+        LEFT -> DOWN
+        DOWN -> RIGHT
+        RIGHT -> UP
+    }
+    fun right() = when (this) {
+        UP -> RIGHT
+        RIGHT -> DOWN
+        DOWN -> LEFT
+        LEFT -> UP
+    }
+}
---
+++ src/main/kotlin/solved/bj16567.kt
@@ -0,0 +1,39 @@
+package solved
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (sz, lines) = br.readLine().split(' ').map { it.toInt() }
+    val s = br.readLine().split(' ')
+    var prev = -1
+    var ans = 0
+    val m = Array(sz) {
+        val a = s[it].toInt()
+        if (prev != a && a == 1)
+            ans++
+        prev = a
+        a
+    }
+    val sb = StringBuilder()
+    for (asdf in 0..< lines) {
+        val c = br.readLine().split(' ').map { it.toInt() }
+        when (c[0]) {
+            0 -> sb.append(ans).append('\n')
+            1 -> {
+                val p = c[1] - 1
+                if (m[p] == 1) continue
+                m[p] = 1
+                var o = 0
+                if (p - 1 >= 0 && m[p - 1] == 1)
+                    o++
+                if (p + 1 < sz && m[p + 1] == 1)
+                    o++
+                when (o) {
+                    0 -> ans++ // new
+                    2 -> ans-- // connection
+                }
+            }
+        }
+    }
+    println(sb)
+    br.close()
+}
