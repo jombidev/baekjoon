64835493acab5e18121dab5e624e7a871ceb1ddb
jombi <jombi@duck.com>
2024-04-25 19:01:58 +0900

    24/04/25 - 2 more silvers

---
+++ src/main/kotlin/solved/bj1181.kt
@@ -0,0 +1,5 @@
+package solved
+
+fun main() {
+    print(List(readln().toInt()) { readln() }.distinct().sorted().sortedBy { it.length }.joinToString("\n"))
+}
---
+++ src/main/kotlin/solved/bj1920.kt
@@ -0,0 +1,14 @@
+package solved
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val hashMap = hashMapOf<Int, Boolean>()
+    br.readLine()
+    for (s in br.readLine().split(' ')) {
+        hashMap[s.toInt()] = true
+    }
+    br.readLine()
+    for (c1 in br.readLine().split(' ')) {
+        println(if (hashMap[c1.toInt()] == true) 1 else 0)
+    }
+}
