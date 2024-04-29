63a3fb6ff32d75a7ca2668a96e1b79804b7986da
jombi <jombi@duck.com>
2024-04-29 15:22:52 +0900

    24/04/26 ~ 24/04/29 - 3days streak (frozen once)

---
+++ src/main/kotlin/solved/bj1264.kt
@@ -0,0 +1,9 @@
+package solved
+
+fun main() {
+    val mo = "aeiouAEIOU"
+    var inp: String
+    while (readln().also { inp = it } != "#") {
+        println(inp.count { it in mo })
+    }
+}
--- src/main/kotlin/solved/bj16496.kt
+++ src/main/kotlin/solved/bj16496.kt
@@ -1,9 +1,8 @@
 package solved
 
 fun main() {
-    print(String(System.`in`.readBytes()).split(Regex("\r?\n")).filter { it.isNotBlank() }.sortedWith { a, b ->
-        val a = a.trim()
-        val b = b.trim()
+    readln()
+    println(readln().split(' ').filter { it.isNotBlank() }.sortedWith { a, b ->
         val aFirst = "$a$b".toBigInteger()
         val bFirst = "$b$a".toBigInteger()
         if (aFirst >= bFirst) -1 else 1
