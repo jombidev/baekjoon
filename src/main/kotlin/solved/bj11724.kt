f10c288c17ad5a3ea984fa8884577887d1b5fde4
jombi <jombi@duck.com>
2024-05-02 14:03:29 +0900

    24/05/02 - Silver II - 연결 요소의 개수

---
+++ src/main/kotlin/solved/bj11724.kt
@@ -0,0 +1,42 @@
+package solved
+
+fun main() {
+    val (n, m) = readln().ints()
+    val o = HashMap<Int, MutableList<Int>>()
+    val v = Array(n) { false }
+    for (i in 0 ..< m) {
+        val (a, b) = readln().split(" ").map { it.toInt() - 1 }
+        if (o[a] == null) o[a] = ArrayList()
+        o[a]!!.add(b)
+
+        if (o[b] == null) o[b] = ArrayList()
+        o[b]!!.add(a)
+    }
+
+    var count = 0
+
+    for (i in 0..< n) {
+        if (v[i]) continue
+        if (o[i] == null) {
+            count++
+            continue
+        }
+
+        val q = ArrayList<Int>()
+        q.add(i)
+        while (q.isNotEmpty()) {
+            for (j in o[q.removeFirst()]!!) {
+                if (!v[j]) {
+                    v[j] = true
+                    q.add(j)
+                }
+            }
+        }
+
+        count++
+    }
+
+    print(count)
+}
+
+private fun String.ints() = split(" ").map { it.toInt() }
