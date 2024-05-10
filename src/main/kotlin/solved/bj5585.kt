15fb2f04d280d71de2a501b831655d5d88d2fa0f
jombi <jombi@duck.com>
2024-05-11 01:56:22 +0900

    24/05/11 - Bronze II, Greedy

---
+++ src/main/kotlin/solved/bj5585.kt
@@ -0,0 +1,12 @@
+package solved
+
+fun main() {
+    var n = 1000 - readln().toInt()
+    val a = listOf(500, 100, 50, 10, 5, 1)
+    var c = 0
+    for (i in a.indices) {
+        c += n / a[i]
+        n %= a[i]
+    }
+    print(c)
+}
