38c05ea2f87e8b1114e14366a41b2117c83e97e7
jombi <jombi@duck.com>
2025-11-18 13:51:10 +0900

    251118 :: 15651 - N과 M (3)
    아 시간초과;

---
+++ src/solved/bj15651.kt
@@ -0,0 +1,23 @@
+import java.util.LinkedList
+
+val v = LinkedList<Int>()
+val sb = StringBuilder()
+
+fun main() {
+    val (n, m) = readln().split(' ').map { it.toInt() }
+    val g = Array(n) { it + 1 }
+    bt(g, m)
+    println(sb.toString())
+}
+
+fun bt(g: Array<Int>, limit: Int) {
+    for (i in g.indices) {
+        v.addLast(i)
+        if (v.size == limit) {
+            sb.append(v.map { g[it] }.joinToString(" ")).append("\n")
+        } else {
+            bt(g, limit)
+        }
+        v.removeLast()
+    }
+}
