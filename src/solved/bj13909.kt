0f7c9288f2af3fd457427cde7248f6a0d67dce15
jombi <jombi@duck.com>
2025-11-18 09:29:22 +0900

    251117 :: 13909 - 창문 닫기

---
+++ src/solved/bj13909.kt
@@ -0,0 +1,13 @@
+fun main()=print(Math.sqrt(readln().toInt()+.0).toInt())
+//fun main() {
+//    val t = readln().toInt()
+//    val a = Array(t + 1) { true }
+//    for (i in 2..t)
+//        for (j in i..t step i) {
+//            a[j] = !a[j]
+//        }
+//
+//    var i = 0
+//    println(a.joinToString(", ") { (i++).toString().padStart(if (it) 4 else 5, ' ') })
+//    println(a.contentToString())
+//}
