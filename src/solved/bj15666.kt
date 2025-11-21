7cd3b5b2ee631552f1b557bdc914c6b61b5b24f5
jombi <jombi@duck.com>
2025-11-21 11:57:01 +0900

    251121 :: 15666 - N과 M (12)

---
+++ src/solved/bj15666.kt
@@ -0,0 +1,26 @@
+import java.util.LinkedList
+
+val v = LinkedList<Int>()
+val ls = LinkedHashSet<String>()
+
+fun main() {
+    val (_, m) = readln().split(' ').map { it.toInt() }
+    val g = readln().split(' ').map { it.toInt() }.sorted().toIntArray()
+    bt(g, m)
+    print(ls.joinToString("\n"))
+}
+
+fun bt(g: IntArray, limit: Int) {
+    for (i in g.indices) {
+        if (v.any { it > i })
+            continue
+        v.addLast(i)
+        if (v.size == limit) {
+            val curr = v.map { g[it] }.joinToString(" ")
+            ls.add(curr)
+        } else {
+            bt(g, limit)
+        }
+        v.removeLast()
+    }
+}
