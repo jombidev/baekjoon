d6adaeb2f639e993ee3e406530b2cc8a04fd17c2
jombi <jombi@duck.com>
2025-11-21 11:25:10 +0900

    251121 :: 15656 - N과 M (7)

---
+++ src/solved/bj15656.kt
@@ -0,0 +1,23 @@
+import java.util.LinkedList
+
+val v = LinkedList<Int>()
+val sb = StringBuilder()
+
+fun main() {
+    val (_, m) = readln().split(' ').map { it.toInt() }
+    val g = readln().split(' ').map { it.toInt() }.sorted().toIntArray()
+    bt(g, m)
+    print(sb.toString())
+}
+
+fun bt(g: IntArray, limit: Int) {
+    for (i in g.indices) {
+        v.addLast(i)
+        if (v.size == limit) {
+            sb.append(v.map { g[it] }.joinToString(" ")).append('\n')
+        } else {
+            bt(g, limit)
+        }
+        v.removeLast()
+    }
+}
