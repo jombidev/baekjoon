3dddc13c25a1d8c6341b571b95c3583169e241c5
jombi <jombi@duck.com>
2025-12-23 13:26:42 +0900

    251223 :: 11444 - 피보나치 수 6

---
+++ src/solved/bj11444.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val mod = 1_000_000_007
+    val n = readln().toLong()
+
+    val m = HashMap<Long, Long>()
+
+    fun fib(n: Long): Long {
+        if (m[n] != null)
+            return m[n]!!
+
+        if (n == 1L || n == 0L)
+            return n
+
+        if (n % 2 == 0L) {
+            val l = fib(n / 2) % mod
+            val r = fib(n / 2 - 1) % mod
+            m[n] = l * (l + 2 * r) % mod
+        } else {
+            val l = fib((n + 1) / 2) % mod
+            val r = fib((n - 1) / 2) % mod
+            m[n] = (l * l + r * r) % mod
+        }
+
+        return m[n]!! % mod
+    }
+
+    print(fib(n))
+}
