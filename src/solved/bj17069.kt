bfdb903980342f121473772354138ddc79ab5a0f
jombi <jombi@duck.com>
2025-12-23 15:29:23 +0900

    251223 :: 17069 - 파이프 옮기기 2
    메모제이션

---
+++ src/solved/bj17069.kt
@@ -0,0 +1,57 @@
+fun main() {
+    data class P(val x: Int, val y: Int, val s: Int) {
+        val noi
+            get() = when (s) {
+                0 -> 1
+                1 -> 0
+                else -> -1
+            }
+    }
+
+    val sx = arrayOf(1, 0, 1)
+    val sy = arrayOf(0, 1, 1)
+
+    val n = readln().toInt()
+    val r = 0..<n
+
+    val a = Array(n) { readln().split(' ').map { it.toInt() }.toIntArray() }
+    if (a[n - 1][n - 1] == 1)
+        return print(0)
+    val m = Array(3) { Array(n) { LongArray(n) { -1L } } }
+
+    fun dfs(p: P): Long {
+
+        if (m[p.s][p.y][p.x] != -1L)
+            return m[p.s][p.y][p.x]
+
+        val dx = p.x + sx[p.s]
+        val dy = p.y + sy[p.s]
+
+        if (p.s == 2) {
+            for (i in 0..<3) {
+                val ox = p.x + sx[i]
+                val oy = p.y + sy[i]
+
+                if (p.x !in r || p.y !in r || a[p.y][p.x] == 1 || ox !in r || oy !in r || a[oy][ox] == 1)
+                    return 0
+            }
+        } else {
+            if (p.x !in r || p.y !in r || dx !in r || dy !in r || a[p.y][p.x] == 1 || a[dy][dx] == 1)
+                return 0
+        }
+
+        if (dx == dy && dx == n - 1)
+            return 1
+
+        m[p.s][p.y][p.x] = 0
+
+        for (i in 0..<3) {
+            if (p.noi == i) continue
+
+            m[p.s][p.y][p.x] += dfs(P(dx, dy, i))
+        }
+        return m[p.s][p.y][p.x]
+    }
+
+    print(dfs(P(0, 0, 0)))
+}
