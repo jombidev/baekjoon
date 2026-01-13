bda0fc452f608daec54b8ebd5281023884519241
jombi <jombi@duck.com>
2026-01-13 10:22:54 +0900

    260113 :: 11401 - 이항 계수 3

---
+++ src/solved/bj11401.kt
@@ -0,0 +1,22 @@
+const val MOD = 1_000_000_007L
+infix fun Long.t(n: Long) = times(n) % MOD
+
+fun main() {
+    fun exp(n: Long, k: Long): Long {
+        if (k == 0L) return 1L
+        val he = exp(n, k.ushr(1))
+        return if (k and 1L == 1L) he t he t n else he t he
+    }
+
+    fun modp(n: Long) = exp(n, MOD - 2)
+
+    val (n, k) = readln().split(' ').map { it.toInt() }
+    val f = LongArray(n + 2)
+    f[0] = 1
+    f[1] = 1
+
+    for (i in 2..n)
+        f[i] = f[i - 1] t i.toLong()
+
+    print(f[n] t modp(f[k]) t modp(f[n - k]))
+}
