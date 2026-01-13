f8beb70b4774384c9d74af7edfdc949c34a49029
jombi <jombi@duck.com>
2026-01-13 17:31:21 +0900

    260113 :: 11443 - 짝수번째 피보나치 수의 합

---
+++ src/solved/bj11443.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val mod = 1_000_000_007
+    var n = readln().toLong()
+
+    val m = HashMap<Long, Long>()
+    fun fib(n: Long): Long {
+        if (m[n] != null)
+            return m[n]!!
+
+        if (n == 1L || n == 0L)
+            return n
+
+        if (n % 2 == 0L) {
+            val l = fib(n / 2) % mod
+            val r = fib(n / 2 - 1) % mod
+            m[n] = l * (l + 2 * r) % mod
+        } else {
+            val l = fib((n + 1) / 2) % mod
+            val r = fib((n - 1) / 2) % mod
+            m[n] = (l * l + r * r) % mod
+        }
+
+        return m[n]!! % mod
+    }
+
+    if (n % 2 == 1L)
+        n -= 1
+
+    print((fib(n + 1) - fib(2)) % mod)
+}
