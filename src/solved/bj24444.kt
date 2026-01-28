16ab8623c9e8e7aefba592c786d8263db242caf3
jombi <jombi@duck.com>
2026-01-28 11:05:03 +0900

    260128 :: 24444 - 알고리즘 수업 - 너비 우선 탐색 1

---
+++ src/solved/bj24444.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+fun main() {
+    val (n, m, r) = readln().split(' ').map { it.toInt() }
+    class Node(val edges: MutableList<Int>)
+    val e = Array(n + 1) { Node(LinkedList()) }
+    val vis = IntArray(n + 1)
+
+    repeat(m) {
+        val (l, r) = readln().split(' ').map { it.toInt() }
+        e[l].edges.add(r)
+        e[r].edges.add(l)
+    }
+
+    e.forEach { it.edges.sort() }
+
+    var knock = 1
+    vis[r] = 1
+    val ll = LinkedList<Int>()
+    ll.add(r)
+
+    while (ll.isNotEmpty()) {
+        val o = ll.removeFirst()
+
+        for (i in e[o].edges) {
+            if (vis[i] != 0) continue
+            vis[i] = ++knock
+            ll.add(i)
+        }
+    }
+
+    print(vis.drop(1).joinToString(" "))
+}
