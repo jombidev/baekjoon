d956bacc653cc62f936d12292eca965fce083c87
jombi <jombi@duck.com>
2026-01-28 11:04:38 +0900

    260128 :: 24479 - 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ src/solved/bj24479.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+fun main() {
+    val (n, m, r) = readln().split(' ').map { it.toInt() }
+    class Node(val edges: MutableList<Int>)
+    val e = Array(n + 1) { Node(LinkedList()) }
+    val vis = IntArray(n + 1)
+
+    repeat(m) {
+        val (l, r) = readln().split(' ').map { it.toInt() }
+        e[l].edges.add(r)
+        e[r].edges.add(l)
+    }
+
+    e.forEach { it.edges.sort() }
+
+    var knock = 0
+
+    fun dfs(o: Int) {
+        vis[o] = ++knock
+
+        for (i in e[o].edges) {
+            if (vis[i] != 0) continue
+            dfs(i)
+        }
+    }
+
+    dfs(r)
+    print(vis.drop(1).joinToString(" "))
+}
