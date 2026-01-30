31c1b3cef3f01ece5636a348a870c765e65b3d32
jombi <jombi@duck.com>
2026-01-30 16:46:16 +0900

    260130 :: 2252 - 줄 세우기

---
+++ src/solved/bj2252.kt
@@ -0,0 +1,29 @@
+fun main() = System.`in`.bufferedReader().use {
+    fun r() = it.readLine()
+    val (n, m) = r().split(' ').map { it.toInt() }
+    val edges = Array(n) { ArrayList<Int>() }
+    val ic = IntArray(n) { 0 }
+
+    repeat(m) {
+        val (s, e) = r().split(' ').map { it.toInt() - 1 }
+        edges[s].add(e)
+        ic[e]++
+    }
+
+    val q = ArrayDeque<Int>()
+    val s = ArrayDeque<Int>()
+
+    for (i in ic.indices) if (ic[i] == 0) q.add(i)
+
+    while (q.isNotEmpty()) {
+        val v = q.removeFirst()
+        s.add(v)
+        for (i in edges[v]) {
+            if (--ic[i] == 0) {
+                q.add(i)
+            }
+        }
+    }
+
+    print(s.joinToString(" ") { "${it + 1}" })
+}
