e987885b3c8a828141a0dc925ff3a885e725d0c6
Jombi <me@jombi.dev>
2026-02-25 10:56:33 +0900

    260225 :: 12764 - 싸지방에 간 준하
    왜 자꾸 안푼거 solved에 들어있냐고...

--- src/solved/bj12764.kt
+++ src/solved/bj12764.kt
@@ -1,26 +1,33 @@
-package solved//import java.util.PriorityQueue
-//
-//fun solved.solved.solved.solved.solved.main() {
-//    val r = System.`in`.bufferedReader()
-//    val N = r.readLine().toInt()
-//    val us = Array(N) { r.readLine().split(' ').map { it.toInt() } }
-//    us.sortBy { it[0] }
-//    val pcs = Array(N) { 0 }
-//    val using = PriorityQueue<Pair<Int, Int>> { i, j -> i.second.compareTo(j.second) }
-//    while (us.isNotEmpty()) {
-//        val (start, end) = us.remove()
-//        using.remove()
-//        val i = using.indexOf(-1)
-//        pcs[i]++
-//        using[i] = end
-//    }
-//    println(pcs.indexOf(0))
-//    val w = System.out.bufferedWriter()
-//    for (pc in pcs) {
-//        if (pc < 1) break
-//        w.write("$pc ")
-//    }
-//    w.flush()
-//    r.close()
-//    w.close()
-//}
+import java.util.PriorityQueue
+
+fun main() {
+    val n = readln().toInt()
+    val p = ArrayDeque(List(n) { readln().split(' ').map { it.toInt() } })
+
+    p.sortBy { it[0] }
+
+    val pq = PriorityQueue<List<Int>>(compareBy { it[1] })
+    val mt = PriorityQueue<Int>()
+    val sc = IntArray(n)
+    var ms = 0
+
+    while (p.isNotEmpty()) {
+        val (cs, ce) = p.removeFirst()
+        var pk = pq.peek()
+
+        while (pk != null && pk[1] < cs) {
+            pq.remove()
+            mt.add(pk[2])
+
+            pk = pq.peek()
+        }
+
+        val ts = mt.poll() ?: pq.size
+        ms = maxOf(ms, ts + 1)
+        sc[ts]++
+        pq.add(listOf(cs, ce, ts))
+    }
+
+    println(ms)
+    print(sc.take(ms).joinToString(" "))
+}
