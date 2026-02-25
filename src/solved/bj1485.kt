57f7564e9be7733677df992f1738498f1d993aa9
Jombi <me@jombi.dev>
2026-02-25 10:20:25 +0900

    260225 :: 1485 - 정사각형
    왜 자꾸 안푼거 solved에 들어있냐고

--- src/solved/bj1485.kt
+++ src/solved/bj1485.kt
@@ -1,14 +1,20 @@
-fun main() {
-    for (i in 0 until  readln().toInt()) {
-        val points = List(4) { readln().split(' ').map { it.toInt() + 100000 } }
-        val minX = points.minOf { it[0] }
-        val minY = points.minOf { it[1] }
-        val pMap = points.sortedBy { it[0]+it[1] }.map { listOf(it[0] - minX, it[1] - minY) }
-        val ps = pMap.map { it.sorted() }
-        println(if (pMap.any { it.all { it == 0 } }) {
-            if (ps[1] == ps[2]) 1 else 0
-        } else {
-            if (ps[0] == ps[1] && ps[2] == ps[3]) 1 else 0
-        })
-    }
+fun main() = repeat(readln().toInt()) {
+    val points = Array(4) { readln().split(' ').map { it.toInt() } }
+    points.sortWith(compareBy<List<Int>> { it[0] }.thenBy { it[1] })
+
+    fun pow2(n: Int) = n * n
+    fun dist(a: List<Int>, b: List<Int>) = kotlin.math.sqrt(.0 + pow2(b[0] - a[0]) + pow2(b[1] - a[1]))
+
+    val straight = setOf(
+        dist(points[0], points[1]),
+        dist(points[1], points[3]),
+        dist(points[3], points[2]),
+        dist(points[2], points[0]),
+    ).size == 1
+    val diag = setOf(
+        dist(points[0], points[3]),
+        dist(points[1], points[2]),
+    ).size == 1
+
+    println(if (straight && diag) 1 else 0)
 }
